f3e97618d37c6bc748642ef07812ba5b0d62f02f
thomastran117 <thomas.tran.1591@gmail.com>
2025-09-03 06:54:05 -0400

    adjusted logger

--- backend/middleware/httpLogger.js
+++ backend/middleware/httpLogger.js
@@ -1,11 +1,28 @@
+const chalk = require("chalk");
+
 function requestLogger(req, res, next) {
   const start = Date.now();
 
   res.on("finish", () => {
     const duration = Date.now() - start;
+    let statusColor;
+
+    if (res.statusCode >= 500) {
+      statusColor = chalk.red;
+    } else if (res.statusCode >= 400) {
+      statusColor = chalk.yellow;
+    } else if (res.statusCode >= 300) {
+      statusColor = chalk.cyan;
+    } else if (res.statusCode >= 200) {
+      statusColor = chalk.green;
+    } else {
+      statusColor = chalk.white;
+    }
+
     const log =
       `[${new Date().toISOString()}] ${req.method} ${req.originalUrl} ` +
-      `${res.statusCode} - ${duration}ms`;
+      `${statusColor(res.statusCode)} - ${duration}ms`;
+
     console.log(log);
   });
 
