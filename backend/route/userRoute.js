7dfcc1eaa95ccd616076eb8215bd444b0d413c24
thomastran117 <thomas.tran.1591@gmail.com>
2025-09-05 02:25:24 -0400

    Created users endpoint

---
+++ backend/route/userRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { updateUser, deleteUser, getStudentByCourse, getTeacherByCourse, getUser } = require("../controller/userController");
+
+const router = express.Router();
+
+router.put("/:id", updateUser)
+router.delete("/:id", deleteUser)
+router.get("/course-t/id", getTeacherByCourse);
+router.get("/course-s/:id", getStudentByCourse);
+router.get("/:id", getUser)
+
+module.exports = router;
