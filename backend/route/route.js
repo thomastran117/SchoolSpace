9c95df742cdc0f63547d7fe7b2d49d32912e2c6b
thomastran117 <thomas.tran.1591@gmail.com>
2025-09-05 02:25:52 -0400

    Connected endpoints to main route file

--- backend/route/route.js
+++ backend/route/route.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const authRoute = require("./authRoute");
+const couresRoute = require("./courseRoute");
+const enrollRoute = require("./enrollRoute")
+const userRoute = require("./userRoute");
 
 const router = express.Router();
+
 router.use("/auth", authRoute);
+router.use("/courses", couresRoute);
+router.use("/course-enroll", enrollRoute)
+router.use("/user", userRoute);
+
 module.exports = router;
