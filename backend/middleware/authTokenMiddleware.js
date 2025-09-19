c432d27f06c6e859e5b80e5e8c172c77e50e9d04
thomastran117 <thomas.tran.1591@gmail.com>
2025-09-19 00:18:56 -0400

    Added API Key

--- backend/middleware/authTokenMiddleware.js
+++ backend/middleware/authTokenMiddleware.js
@@ -1,12 +1,35 @@
 const { getUserPayload } = require("../service/tokenService");
+const config = require("../config/envManager");
+
+const CLIENT = config.frontend_client;
+const API_KEY = config.schoolspace_api_key;
 
 const makeRequireAuth = async (req, res, next) => {
   try {
+    const origin = req.headers.origin;
+    const apiKey = req.headers["schoolspace-api-key"];
     const authHeader = req.headers.authorization;
-    req.user = await getUserPayload(authHeader);
-    next();
+
+    if (origin === CLIENT) {
+      if (!authHeader) {
+        return res.status(401).json({ error: "Missing Authorization header" });
+      }
+      req.user = await getUserPayload(authHeader);
+      return next();
+    }
+
+    if (apiKey === API_KEY) {
+      if (!authHeader) {
+        return res.status(401).json({ error: "Missing Authorization header" });
+      }
+      req.user = await getUserPayload(authHeader); 
+      // TODO: later map apiKey → user/service identity
+      return next();
+    }
+
+    return res.status(403).json({ error: "Forbidden" });
   } catch (e) {
-    res
+    return res
       .status(e.statusCode || 401)
       .json({ error: e.message || "Unauthorized" });
   }
