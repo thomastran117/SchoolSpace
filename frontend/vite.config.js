ea3517ef7cba1a6aad45526adb06381a711f0a2d
thomastran117 <thomas.tran.1591@gmail.com>
2025-10-07 20:20:13 -0400

    Removed uno back to bootstrap

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), UnoCSS()],
+  plugins: [react()],
   server: {
     port: 3040,
   },
