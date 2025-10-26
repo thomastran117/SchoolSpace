3031e94240cd41c4130e79682ce1ea46ac0e4d72
thomastran117 <thomas.tran.1591@gmail.com>
2025-10-26 11:31:31 -0400

    backend typescript start

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,8 +8,8 @@
  * @version 1.0.0
  *
  */
-
 import app from "./app.js";
 import logger from "./utility/logger.js";
+
 const port = process.env.PORT || 8040;
 app.listen(port, () => logger.info(`Express server has started`));
