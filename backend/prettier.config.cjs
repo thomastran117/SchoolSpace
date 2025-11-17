4bbe618d562ba61c69a1c5df163497b9200dab61
thomastran117 <thomas.tran.1591@gmail.com>
2025-11-16 23:43:40 -0500

    updated prettier

--- backend/prettier.config.cjs
+++ backend/prettier.config.cjs
@@ -1,13 +1,3 @@
 module.exports = {
   plugins: ["prettier-plugin-organize-imports"],
-
-  importOrder: [
-    "^node:", // Node built-ins (fs, path, crypto…)
-    "^[a-zA-Z]", // npm packages
-    "^@/.*$", // absolute imports (if you have tsconfig paths)
-    "^[./]", // relative imports ("./controller/...")
-  ],
-
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
 };
