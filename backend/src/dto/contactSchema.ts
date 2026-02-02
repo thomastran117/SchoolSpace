e282e61bb830100729986e1d6c24118d717cc946
thomastran117 <thomas.tran.1591@gmail.com>
2026-02-02 01:45:04 -0500

    Added Contact Dtos

---
+++ backend/src/dto/contactSchema.ts
@@ -0,0 +1,25 @@
+/**
+ * @file contactSchema.ts
+ * @description
+ * Defines the DTOs for the Contact controller
+ *
+ * @module dto
+ * @version 1.0.0
+ * @auth Thomas
+ */
+import { z } from "zod";
+
+const CreateContactSchema = z.object({
+  email: z.string().trim().toLowerCase().email().max(100),
+  topic: z.string().min(4).max(100),
+  message: z.string().min(10).max(1000),
+  captcha: z.string().trim().min(1).max(4000)
+});
+
+const UpdateContactSchema = CreateContactSchema.partial();
+
+type CreateContactDto = z.infer<typeof CreateContactSchema>;
+type UpdateContactDto = z.infer<typeof UpdateContactSchema>;
+
+export type { CreateContactDto, UpdateContactDto };
+export { CreateContactSchema, UpdateContactSchema };
